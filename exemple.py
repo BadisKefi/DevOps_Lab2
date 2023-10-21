fb540fb0644e556ab2cb3a5bcf466b2a7a605d6e
BadisKefi <badiskefi@gmail.com>
2023-10-21 21:22:39 +0200

    badis testing again without mergin to master commit

--- exemple.py
+++ exemple.py
@@ -2,3 +2,4 @@ print ("Hello World");
 print ("Feature 1 modification");
 print ("Feature 2 modification");
 print ("badis testing line act as you didn't see this");
+print ("badis testing again line act as you didn't see this");
