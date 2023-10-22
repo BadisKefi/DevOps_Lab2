b2ac43f02361de2e3a0d37dcfbe83a2fa8be96dd
nada <nada.hawes002@gmail.com>
2023-10-22 17:27:07 +0100

    feature3 commit

--- exemple.py
+++ exemple.py
@@ -3,3 +3,4 @@ print ("Feature 1 modification");
 print ("Feature 2 modification");
 print ("badis testing line act as you didn't see this");
 print ("badis testing again line act as you didn't see this");
+print("feature 3 modification")
